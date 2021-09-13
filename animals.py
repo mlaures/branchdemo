25115c2055e53ec5f3f2174a066bae4b7fd79731
Mei-Ling <mlaures@caltech.edu>
2021-09-13 09:56:48 -0700

    Print cat and dog sounds

---
+++ animals.py
@@ -0,0 +1,18 @@
+import sys
+if sys.version_info < (3,):
+    input = raw_input
+
+def sounds(animal):
+    if animal == "cat":
+        print("meow")
+    elif animal == "dog":
+        print("woof")
+    else:
+        print("are you sure that's an animal?")
+
+def main():
+    animal = input("which animal?: ")
+    sounds(animal)
+
+if __name__=="__main__":
+    main()
